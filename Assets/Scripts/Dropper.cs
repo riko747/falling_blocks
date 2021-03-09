fedd2e40bea3c4479852f89a618f9ae4caf194b9
Vitaliy Bosiy <vitaliybosiy@gmail.com>
2021-03-09 22:17:32 +0200

    Added circle movement and falling blocks;

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField]GameObject circle;
+    static int seconds = 1;
+    Rigidbody rigidBody;
+    static float fallSpeed = -1f;
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+        rigidBody.AddForce(0, fallSpeed, 0, ForceMode.Impulse);
+        Invoke("SpawnCube", 1f);
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.time % 5 == 0)
+            rigidBody.AddForce(0, fallSpeed, 0, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Destroy(gameObject);
+        else if (collision.gameObject.tag == "Player")
+        {
+            Time.timeScale = 0;
+            Destroy(circle);
+        }
+    }
+
+    void SpawnCube()
+    {
+        if (seconds == 5)
+        {
+            fallSpeed += -0.5f;
+            print(fallSpeed);
+            seconds = 0;
+        }
+        print("Seconds: " + seconds);
+        seconds++;
+        Vector3 spawnPosition = new Vector3(Random.Range(-5.5f, 5.5f), 9f, -3.5f);
+        Instantiate(this, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    Vector3 moveLeft, moveRight;
+
+    float x;
+    void Start()
+    {
+        x = 5 * Time.deltaTime;
+        moveLeft = new Vector3(-x, 0, 0);
+        moveRight = new Vector3(x, 0, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) //&& Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            double halfScreen = Screen.width / 2.0;
+
+            if (touchPosition.x < halfScreen)
+            {
+                transform.Translate(Vector3.left * 5f * Time.deltaTime);
+            }
+            else if (touchPosition.x > halfScreen)
+            {
+                transform.Translate(Vector3.right * 5f * Time.deltaTime);
+            }
+        }
+    }
+}
